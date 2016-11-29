2aad072714ffb6bdb2fe8657e66cd896dfff0470
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-11-29 17:48:03 +0900

    Update MIDI.cpp

--- src/MIDI.cpp
+++ src/MIDI.cpp
@@ -3,7 +3,7 @@
 #define btn2  10  // 버튼2의 아두이노 핀번호 정의
 #define btn3  9   // 버튼3의 아두이노 핀번호 정의
 #define btn4  8   // 버튼4의 아두이노 핀번호 정의
-#define defaultPatch 15 //악기 초기화 버튼 설정 악기번호
+// 3:midi rx , 4:midi reset  아두이노 핀 3번 4번은 이미 사용중
 
 SoftwareSerial mySerial(2, 3); //SW시리얼핀 정의 D3이 MIDI신호 전송용,  D2는 미사용 
 
@@ -20,43 +20,143 @@ boolean br1;  // 버튼1 상태 확인용 입력값 임시저장용
 boolean br2;  // 이하, 위와 유사
 boolean br3;
 boolean br4;
+int check = 0;
+int patch = 0; //악기 대응, 연주될 악기 종류 (0~127: 기본 128 가지 선택가능)
 
-//int patch = 0; //악기 대응, 연주될 악기 종류 (0~127: 기본 128 가지 선택가능)
-
-int c = 60; //버튼1의  note(음계)  가령 "도"  0~127까지 지정가능 (정확한 음계 설정은 MIDI관련정보참고)
-int d = 62; //버튼2의  note(음계)  가령 "레"
-int e = 64; //버튼3의  note(음계)  가령 "미"
-int f = 65; //버튼4의  note(음계)  가령 "파"
-int g = 67; //버튼5의  note(음계)  가령 "솔"
-int a = 69; //버튼6의  note(음계)  가령 "라"
-int b = 71; //버튼7의  note(음계)  가령 "시"
-//int bn8 = 72; //버튼8의  note(음계)  가령 "도~"
+int bn1 = 60; //버튼1의  note(음계)  가령 "도"  0~127까지 지정가능 (정확한 음계 설정은 MIDI관련정보참고)
+int bn2 = 62; //버튼2의  note(음계)  가령 "레"
+int bn3 = 64; //버튼3의  note(음계)  가령 "미"
+int bn4 = 65; //버튼4의  note(음계)  가령 "파"
+int bn5 = 67; //버튼4의  note(음계)  가령 "파"
+int bn6 = 69; //버튼4의  note(음계)  가령 "파"
+int bn7 = 71; //버튼4의  note(음계)  가령 "파"
 
 byte byteData;
-
-void noteOn(byte channel, byte note, byte attack_velocity);
-void noteOff(byte channel, byte note, byte release_velocity);
-void talkMIDI(byte cmd, byte data1, byte data2);
-
+ int setNote1(boolean ls1){
+    if (ls1 == true)
+      return 1;
+   else 
+   return 0;
+  }
+  int setNote2(boolean ls2){
+    if (ls2 == true)
+      return 10;
+   else 
+   return 0;
+  }
+  int setNote3(boolean ls3){
+    if (ls3 == true)
+      return 100;
+   else 
+   return 0;
+  }
+  int setNote4( boolean ls4){
+    if (ls4 == true)
+      return 1000;
+   else 
+   return 0;
+  }
 void setup() {
-	Serial.begin(31250);
+  Serial.begin(31250);
 
-	//미디컨트롤을 위한 소프트웨어 시리얼을 준비합니다.
-	mySerial.begin(31250);
-	//mySerial2.begin(57600);
+  //미디컨트롤을 위한 소프트웨어 시리얼을 준비합니다.
+  mySerial.begin(31250);
   
-	//VS1053를 초기화하고 사용할 준비를 합니다.
-	pinMode(resetMIDI, OUTPUT);
-	digitalWrite(resetMIDI, LOW);
-	delay(100);
-	digitalWrite(resetMIDI, HIGH);
-	delay(100);
+  //VS1053를 초기화하고 사용할 준비를 합니다.
+  pinMode(resetMIDI, OUTPUT);
+  digitalWrite(resetMIDI, LOW);
+  delay(100);
+  digitalWrite(resetMIDI, HIGH);
+  delay(100);
   
-	pinMode( btn1, INPUT);      // 버튼1 입력용 핀모드를  입력모드로 전환
-	digitalWrite( btn1, HIGH);  // 내부 PullUp 설정, 스위치의 나머지 한선은 GND에 물리면 됩니다.(초간단)
-	pinMode( btn2, INPUT);      // 이하, 위와 유사
-	digitalWrite( btn2, HIGH);  // 버튼을 누르지 않았을때 플로팅 상태가 되기 때문에 0과 1을 완전히 구분시켜주기 위하여 내부 풀업저항을 사용한다.
-	pinMode( btn3, INPUT);
+  pinMode( btn1, INPUT);      // 버튼1 입력용 핀모드를  입력모드로 전환
+  digitalWrite( btn1, HIGH);  // 내부 PullUp 설정, 스위치의 나머지 한선은 GND에 물리면 됩니다.(초간단)
+  pinMode( btn2, INPUT);      // 이하, 위와 유사
+  digitalWrite( btn2, HIGH);
+  pinMode( btn3, INPUT);
+  digitalWrite( btn3, HIGH);
+  pinMode( btn4, INPUT);
+  digitalWrite( btn4, HIGH);
+}
+
+void loop() {
+    br1 = digitalRead(btn1);
+    br2 = digitalRead(btn2);
+    br3 = digitalRead(btn3);
+    br4 = digitalRead(btn4);
+
+    check = setNote1(br1) + setNote2(br2) + setNote3(br3) + setNote4(br4);
+   if(check==1000){
+     noteOn(0, bn1,100);
+     Serial.println("1");
+     delay(500);
+     noteOff(0, bn1,0);
+   }
+   else if(check==111){
+     noteOn(0, bn2,100);
+     Serial.println("2");
+     delay(500);
+     noteOff(0, bn2,0);
+   } 
+   else if(check==100){
+     noteOn(0, bn3,100);
+     Serial.println("3");
+     delay(500);
+     noteOff(0, bn3,0);
+   }
+   else if(check==101){
+     noteOn(0, bn4,100);
+     Serial.println("4");
+     delay(500);
+     noteOff(0, bn4,0);
+   }else if(check==1110){
+     noteOn(0, bn5,100);
+     Serial.println("5");
+     delay(500);
+     noteOff(0, bn5,0);
+   }else if(check==11){
+     noteOn(0, bn6,100);
+     Serial.println("6");
+     delay(500);
+     noteOff(0, bn6,0);
+   }else if(check==1111){
+     noteOn(0, bn7,100);
+     Serial.println("7");
+     delay(500);
+     noteOff(0, bn7,0);
+   }
+  if(Serial.available() > 0)
+  {
+    byteData =  Serial.read();
+    mySerial.write( byteData);
+  }  
+  delay(50);
+}
+
+//노트 온 미디 메세지를 송출합니다. 버튼이 눌린상태와 같습니다.
+//채널 범위는 0-15 입니다.
+void noteOn(byte channel, byte note, byte attack_velocity) {
+  talkMIDI( (0x90 | channel), note, attack_velocity);
+}
+
+//노트 오프 미디 메세지를 송출합니다. 버튼이 눌리지 않은 상태와 같습니다.
+void noteOff(byte channel, byte note, byte release_velocity) {
+  talkMIDI( (0x80 | channel), note, release_velocity);
+}
+
+
+void talkMIDI(byte cmd, byte data1, byte data2) {
+  digitalWrite(ledPin, HIGH);
+  mySerial.write(cmd );
+  mySerial.write(data1 );
+
+  //모든 명령은 1바이트를 지니며, 모든 cmds는   0xBn보다  2 데이터 바이트를 덜 지닙니다.  
+  //(sort of: http://253.ccarh.org/handout/midiprotocol/)
+  if( (cmd & 0xF0) <= 0xB0)
+    mySerial.write(data2 );
+
+  digitalWrite(ledPin, LOW);
+}
 	digitalWrite( btn3, HIGH);
 	pinMode( btn4, INPUT);
 	digitalWrite( btn4, HIGH);
