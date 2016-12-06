6b837c879563b3966252ea3d4f577ad5b44fa1b6
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-12-06 16:45:05 +0900

    cds

--- src/CDS.cpp
+++ src/CDS.cpp
@@ -1,14 +1,26 @@
-#define potpin A0  //set analog interface #0 to connect photocell
-int ledpin=13; 
-int val=0;  //define variable val 
-void setup() 
-{ 
-  pinMode(ledpin,OUTPUT);//set digital interface #11 as output 
-  Serial.begin(9600);//set baud rate as 9600 
-} 
-void loop() 
-{ 
-  val=analogRead(potpin);//read analog 
-  Serial.println(val); 
-  delay(1000);//delay 0.01 s 
+#define laz1 A0   //조도센서1의 아두이노 핀번호 정의
+#define laz2 A1   //조도센서2의 아두이노 핀번호 정의
+#define laz3 A2   //조도센서3의 아두이노 핀번호 정의
+#define laz4 A3   //조도센서4의 아두이노 핀번호 정의
+
+int lazVal1 = 0;  //조도센서1 에서 받은 AnalogRead 값
+int lazVal2 = 0;  //이하, 위와 유사  
+int lazVal3 = 0;  
+int lazVal4 = 0;  
+
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  lazVal1 = analogRead(laz1); //read analog 
+  lazVal2 = analogRead(laz2); //read analog 
+  lazVal3 = analogRead(laz3); //read analog 
+  lazVal4 = analogRead(laz4); //read analog 
+  
+  Serial.println(lazVal1);
+  Serial.println(lazVal2);
+  Serial.println(lazVal3);
+  Serial.println(lazVal4);
+
+  delay(50);
 }
