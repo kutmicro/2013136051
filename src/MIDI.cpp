29599dd99a48a4549622d98b009e3a89acb9210f
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-12-07 18:16:20 +0900

    midi 모듈 테스트

--- src/MIDI.cpp
+++ src/MIDI.cpp
@@ -17,114 +17,26 @@ boolean br3;
 boolean br4;
 int check = 0;
 int patch = 0; //악기 대응, 연주될 악기 종류 (0~127: 기본 128 가지 선택가능)
-
-int note60 = 60; //버튼1의  note(음계)  가령 "도"  0~127까지 지정가능 (정확한 음계 설정은 MIDI관련정보참고)
-int note62 = 62; //버튼2의  note(음계)  가령 "레"
-int note64 = 64; //버튼3의  note(음계)  가령 "미"
-int note65 = 65; //버튼4의  note(음계)  가령 "파"
-int note67 = 67; //버튼4의  note(음계)  가령 "솔"
-int note69 = 69; //버튼4의  note(음계)  가령 "라"
-int note71 = 71; //버튼4의  note(음계)  가령 "시"
-
-byte byteData;
-
-void setup() {
-	Serial.begin(31250);
-
-	//미디컨트롤을 위한 소프트웨어 시리얼을 준비합니다.
-	mySerial.begin(31250);
-
-	//VS1053를 초기화하고 사용할 준비를 합니다.
-	pinMode(resetMIDI, OUTPUT);
-	digitalWrite(resetMIDI, LOW);
-	delay(100);
-	digitalWrite(resetMIDI, HIGH);
-	delay(100);
-
-	pinMode( btn1, INPUT);      // 버튼1 입력용 핀모드를  입력모드로 전환
-	digitalWrite( btn1, HIGH);  // 내부 PullUp 설정, 스위치의 나머지 한선은 GND에 물리면 됩니다.(초간단)
-	pinMode( btn2, INPUT);      // 이하, 위와 유사
-	digitalWrite( btn2, HIGH);
-	pinMode( btn3, INPUT);
-	digitalWrite( btn3, HIGH);
-	pinMode( btn4, INPUT);
-	digitalWrite( btn4, HIGH);
-}
-
-void loop() {
-	br1 = digitalRead(btn1);
-	br2 = digitalRead(btn2);
-	br3 = digitalRead(btn3);
-	br4 = digitalRead(btn4);
-
-	check = setNote1(br1) + setNote2(br2) + setNote3(br3) + setNote4(br4);
-	if(check==1000){
-		noteOn(0, note60, 100);
-		Serial.println("도");
-		delay(500);
-		noteOff(0, note60, 0);
-	}
-	else if(check==100){
-		noteOn(0, note62,100);
-		Serial.println("레");
-		delay(500);
-		noteOff(0, note62,0);
-	} 
-	else if(check==10){
-		noteOn(0, note64,100);
-		Serial.println("미");
-		delay(500);
-		noteOff(0, note64,0);
-	}
-	else if(check==1){
-		noteOn(0, note65,100);
-		Serial.println("파");
-		delay(500);
-		noteOff(0, note65,0);
-	}
-/*	else if(check==1110){
-		noteOn(0, note67,100);
-		Serial.println("솔");
-		delay(500);
-		noteOff(0, note67,0);
-	}else if(check==11){
-		noteOn(0, note69,100);
-		Serial.println("라");
-		delay(500);
-		noteOff(0, note69,0);
-	}else if(check==1111){
-		noteOn(0, note71,100);
-		Serial.println("시");
-		delay(500);
-		noteOff(0, note71,0);
-#include <SoftwareSerial.h>
-#define btn1  11  // 버튼1의 아두이노 핀번호 정의
-#define btn2  10  // 버튼2의 아두이노 핀번호 정의
-#define btn3  9   // 버튼3의 아두이노 핀번호 정의
-#define btn4  8   // 버튼4의 아두이노 핀번호 정의
-// 3:midi rx , 4:midi reset  아두이노 핀 3번 4번은 이미 사용중
-
-SoftwareSerial mySerial(2, 3); //SW시리얼핀 정의 D3이 MIDI신호 전송용,  D2는 미사용 
-
-byte note = 0; //The MIDI연주될 note(음계)
-byte resetMIDI = 4; // VS1053 Reset용 핀
-byte ledPin = 13; //MIDI 트래픽 표시용 LED
-
-boolean br1;  // 버튼1 상태 확인용 입력값 임시저장용
-boolean br2;  // 이하, 위와 유사
-boolean br3;
-boolean br4;
-int check = 0;
-int patch = 0; //악기 대응, 연주될 악기 종류 (0~127: 기본 128 가지 선택가능)
-
-int note60 = 60; //note(음계)  가령 "도"  0~127까지 지정가능 (정확한 음계 설정은 MIDI관련정보참고)
-int note62 = 62; //note(음계)  가령 "레"
-int note64 = 64; //note(음계)  가령 "미"
-int note65 = 65; //note(음계)  가령 "파"
-int note67 = 67; //note(음계)  가령 "솔"
-int note69 = 69; //note(음계)  가령 "라"
-int note71 = 71; //note(음계)  가령 "시"
-int note72 = 72; //note(음계)  가령 "높은도"
+//0~127까지 지정가능 (정확한 음계 설정은 MIDI관련정보참고)
+int note53 = 53; //note(음계) "낮은파"
+int note55 = 55; //note(음계) "낮은솔"
+int note57 = 57; //note(음계) "낮은라"
+int note59 = 59; //note(음계) "낮은시"
+int note60 = 60; //note(음계) "도"
+int note62 = 62; //note(음계) "레"
+int note64 = 64; //note(음계) "미"
+int note65 = 65; //note(음계) "파"
+int note67 = 67; //note(음계) "솔"
+int note69 = 69; //note(음계) "라"
+int note71 = 71; //note(음계) "시"
+int note72 = 72; //note(음계) "높은도"
+int note74 = 74; //note(음계) "높은레"
+int note76 = 76; //note(음계) "높은미"
+int note77 = 77; //note(음계) "높은파"
+int note79 = 79; //note(음계) "높은솔"
+int note81 = 81; //note(음계) "높은라"
+int note83 = 83; //note(음계) "높은시"
+int note84 = 84; //note(음계) "더높은도"
 
 byte byteData;
 
@@ -205,7 +117,49 @@ void loop() {
     Serial.println("높은도");
     delay(200);
     noteOff(note, note72, 0);
-  } 
+  }
+  else if(check==1100){
+    noteOn(note, note74, 100);
+    Serial.println("높은레");
+    delay(200);
+    noteOff(note, note74,0);
+  }
+  else if(check==1101){
+    noteOn(note, note76, 100);
+    Serial.println("높은미");
+    delay(200);
+    noteOff(note, note76, 0);
+  }
+  else if(check==1001){
+    noteOn(note, note77, 100);
+    Serial.println("높은파");
+    delay(200);
+    noteOff(note, note77,0);
+  }
+  else if(check==1010){
+    noteOn(note, note79, 100);
+    Serial.println("높은솔");
+    delay(200);
+    noteOff(note, note79,0);
+  }
+  else if(check==1011){
+    noteOn(note, note81, 100);
+    Serial.println("높은라");
+    delay(200);
+    noteOff(note, note81, 0);
+  }
+  else if(check==101){
+    noteOn(note, note83, 100);
+    Serial.println("높은시");
+    delay(200);
+    noteOff(note, note83,0);
+  }
+  else if(check==110){
+    noteOn(note, note84, 100);
+    Serial.println("더높은도");
+    delay(200);
+    noteOff(note, note84, 0);
+  }
   if(Serial.available() > 0)
   {
     byteData =  Serial.read();
